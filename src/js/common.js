a0bc45877ae4e1abfdd7568092b60ab0d399951d
Alex <lesha.rybalko.1998@mail.ru>
2020-10-27 12:58:53 +0300

    Add brands and footer adaptive

--- src/js/common.js
+++ src/js/common.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function() {
   const mySwiper = new Swiper('.swiper-container', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 2,
     slidesPerGroup: 2,
     spaceBetween: 20,
     speed: 600,
@@ -16,6 +16,20 @@ window.addEventListener('load', function() {
         return `<span class="swiper-dot ${className}"></span>`;
       },
     },
+    breakpoints: {
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 15
+      },
+      890: {
+        slidesPerView: 3,
+        spaceBetween: 15
+      },
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 15
+      },
+    }
   });
 
   const menuToggles = document.querySelectorAll('.toggle-menu');
@@ -26,4 +40,13 @@ window.addEventListener('load', function() {
       menu.classList.toggle('menu_active');
     });
   });
+
+  const $searchToggle = document.querySelector('.header__search-toggle');
+  $searchToggle.addEventListener('click', function() {
+    const $searchParent = $searchToggle.closest('.header__search');
+    console.log($searchParent);
+
+    $searchParent.classList.toggle('header__search_active');
+  });
+  
 });
